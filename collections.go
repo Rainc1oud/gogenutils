a32e9f363d185051b974a200af2500fb2b02df94
ppenguin <hieronymusv@gmail.com>
2021-04-21 23:58:41 +0200

    add dir slices functions

--- collections.go
+++ collections.go
@@ -1,6 +1,39 @@
 package gogenutils
 
-import "reflect"
+import (
+	"os"
+	"reflect"
+	"strings"
+)
+
+// FilterSlicePfxStr filters out all items of which another slice member is a prefix, recursively
+// by convention take the first element as the "pivot"
+func FilterSlicePfxStr(s []string) []string {
+	if len(s) < 2 {
+		return s
+	}
+	r := FilterSlicePfxStr(s[1:])    // recursive except first element ("pivot")
+	r = append([]string{s[0]}, r...) // "insert" s[0] at pos 0 of r
+	for i := 1; i < len(r); i++ {    // first item is reserved as pivot (to compare to)
+		if strings.HasPrefix(r[i], r[0]) {
+			if i < len(r)-1 {
+				r = append(r[0:i], r[i+1:]...) // remove r[i] because it has the prefix
+			} else {
+				r = r[0:i]
+			}
+		}
+	}
+	return r
+}
+
+// FilterCommonRootDirs recursively filters out paths for which any higher root path exists in the same slice
+func FilterCommonRootDirs(strSlice []string) []string {
+	// sanitise trailing slashes
+	for i, s := range strSlice {
+		strSlice[i] = strings.TrimSuffix(s, string(os.PathSeparator))
+	}
+	return FilterSlicePfxStr(strSlice)
+}
 
 // InSlice returns true if item is in list and false otherwise
 func InSlice(item interface{}, slice interface{}) bool {
@@ -32,4 +65,31 @@ func PosInSlice(item interface{}, slice interface{}) int {
 		}
 	}
 	return -1
+}
+
+// Equal tells whether a and b contain the same elements in the same order (!)
+// A nil argument is equivalent to an empty slice.
+func SlicesEqual(a, b interface{}) bool {
+	ai := reflect.ValueOf(a)
+	bi := reflect.ValueOf(b)
+	if ai == bi {
+		return true
+	}
+	if ai.Kind() != reflect.Slice || ai.IsNil() || ai.IsZero() {
+		return false
+	}
+
+	if bi.Kind() != reflect.Slice || bi.IsNil() || bi.IsZero() {
+		return false
+	}
+
+	if ai.Len() != bi.Len() {
+		return false
+	}
+	for i := 0; i < ai.Len(); i++ {
+		if ai.Index(i).Interface() != bi.Index(i).Interface() {
+			return false
+		}
+	}
+	return true
 }
--- collections_test.go
+++ collections_test.go
@@ -23,3 +23,21 @@ func TestInSlice(t *testing.T) {
 	i = 45
 	assert.True(t, InSlice(i, ilst), fmt.Sprintf("%d in %v", i, ilst))
 }
+
+func TestSlicesEqual(t *testing.T) {
+	a := []string{"haha", "hihi", "hoho"}
+	b := []string{"haha", "hihi", "hoho"}
+	assert.True(t, SlicesEqual(a, b), fmt.Sprintf("%v equals %v (element wise)", a, b))
+	b = []string{"haha", "hiha", "hoho"}
+	assert.False(t, SlicesEqual(a, b), fmt.Sprintf("%v ! equals %v (element wise)", a, b))
+}
+
+func TestFilterCommonRootDirs(t *testing.T) {
+	dirs := func() []string { // haha, we need to do this to make sure dirs is a constant!!!
+		return []string{"/var", "/home/john", "/usr/lib/", "/var/lib", "/home/john/Documents", "/usr/lib"}
+	}
+	expected := []string{"/var", "/home/john", "/usr/lib"}
+	filtered := FilterCommonRootDirs(dirs())
+	cmpres := SlicesEqual(expected, filtered)
+	assert.True(t, cmpres, fmt.Sprintf("Filtered %v => %v ?= %v", dirs(), filtered, expected))
+}
